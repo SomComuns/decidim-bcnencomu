e4fbe1110f2052b002a0c44da5c7ec3401663481
elviabth <ejbenedith@gmail.com>
2026-01-19 11:12:05 +0100

    remove consultations migrations

--- db/migrate/20230614125229_add_is_weighted_column_in_decidim_consultations_questions_table.rb
+++
@@ -1,5 +0,0 @@
-class AddIsWeightedColumnInDecidimConsultationsQuestionsTable < ActiveRecord::Migration[6.0]
-  def change
-    add_column :decidim_consultations_questions, :is_weighted, :boolean, default: false
-  end
-end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,5 @@
 
 require "decidim/core/test/factories"
 require "decidim/proposals/test/factories"
-require "decidim/consultations/test/factories"
 require "decidim/decidim_awesome/test/factories"
 require "decidim/meetings/test/factories"
