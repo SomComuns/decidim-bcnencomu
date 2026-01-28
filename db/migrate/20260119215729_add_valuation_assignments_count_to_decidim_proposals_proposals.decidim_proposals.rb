bf07d3e3033b6519b1fd654b9541e3dd023b4d54
Ivan Vergés <ivan@pokecode.net>
2026-01-28 20:08:11 +0100

    fix migrations

--- db/migrate/20260119215729_add_valuation_assignments_count_to_decidim_proposals_proposals.decidim_proposals.rb
+++ db/migrate/20260119215729_add_valuation_assignments_count_to_decidim_proposals_proposals.decidim_proposals.rb
@@ -6,13 +6,13 @@ class AddValuationAssignmentsCountToDecidimProposalsProposals < ActiveRecord::Mi
   def change
     add_column :decidim_proposals_proposals, :valuation_assignments_count, :integer, default: 0
 
-    reversible do |dir|
-      dir.up do
-        Decidim::Proposals::Proposal.reset_column_information
-        Decidim::Proposals::Proposal.unscoped.find_each do |record|
-          Decidim::Proposals::Proposal.reset_counters(record.id, :valuation_assignments)
-        end
-      end
-    end
+    # reversible do |dir|
+    #   dir.up do
+    #     Decidim::Proposals::Proposal.reset_column_information
+    #     Decidim::Proposals::Proposal.unscoped.find_each do |record|
+    #       Decidim::Proposals::Proposal.reset_counters(record.id, :valuation_assignments)
+    #     end
+    #   end
+    # end
   end
 end
